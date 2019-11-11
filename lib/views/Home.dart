f87e31b4205ed751d94463841802d6fe37725ec5
leothon <langmancaizi@gmail.com>
2019-11-12 00:54:53 +0800

    配置首页页面

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -13,6 +13,15 @@ class AppPage extends StatefulWidget {
 }
 
 class _MyAppPageState extends State<AppPage>{
+
+  List<Widget> _widgetList = List();
+  int _currentIndex = 0;
+  List tabData = [
+    {'text':'首页','icon':Icon(Icons.audiotrack)},
+    {'text':'艺条','icon':Icon(Icons.library_books)},
+    {'text':'秀吧','icon':Icon(Icons.ondemand_video)},
+    {'text':'我的','icon':Icon(Icons.account_circle)},
+  ];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
