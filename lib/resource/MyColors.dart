22aed2386cf7bd3bf6c6e5a7988e7d67848d6018
leothon <langmancaizi@gmail.com>
2019-11-14 00:51:51 +0800

    发布页面，未完成动画设计

---
+++ lib/resource/MyColors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class MyColors{
+
+  static const Color colorPrimary = Color(0xff4caf50);
+  static const Color colorPrimaryDark = Color(0xff388E3C);
+  static const Color colorAccent = Color(0xff8BC34A);
+  static const Color colorPrimaryLight = Color(0xffC8E6C9);
+
+  static const Color primaryText = Color(0xff212121);
+  static const Color secondaryText = Color(0xff757575);
+
+  static const Color dividerColor = Color(0xffBDBDBD);
+
+  static const Color alphaWhite = Color(0xE6FFFFFF);
+
+}
--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,4 +1,5 @@
 import 'package:artepie/model/user_info.dart';
+import 'package:artepie/resource/MyColors.dart';
 import 'package:artepie/views/aboutPage/aboutPage.dart';
 import 'package:artepie/views/articlePage/articlePage.dart';
 import 'package:artepie/views/homePage/homePage.dart';
@@ -31,6 +32,21 @@ class _MyAppPageState extends State<AppPage> {
   List<BottomNavigationBarItem> _myTabs = [];
   String appBarTitle;
 
+  List<String> addIconItems = <String>['录课程', '发文章', '秀吧拍', '开直播'];
+  List<Icon> addIcon = [
+    Icon(Icons.videocam, color: Colors.white),
+    Icon(Icons.library_books, color: Colors.white),
+    Icon(Icons.ondemand_video, color: Colors.white),
+    Icon(Icons.live_tv, color: Colors.white)
+  ];
+
+  List<Color> addIconColors = [
+    Colors.lightGreen,
+    Colors.blue,
+    Colors.red,
+    Colors.deepOrange
+  ];
+  bool addbackShow = false;
   @override
   void initState() {
     super.initState();
@@ -60,41 +76,79 @@ class _MyAppPageState extends State<AppPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _widgetList,
-      ),
-      floatingActionButton: Container(
-        height: 65,
-        width: 65,
-        padding: EdgeInsets.all(8),
-        margin: EdgeInsets.only(top: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(40),
-          color: Colors.white,
 
-        ),
+    return Stack(
+      children: <Widget>[
+        Container(
+            foregroundDecoration: BoxDecoration(
+                color: addbackShow ? MyColors.alphaWhite : Colors.transparent),
+            child: Scaffold(
+              body: IndexedStack(
+                index: _currentIndex,
+                children: _widgetList,
+              ),
+              floatingActionButton: Container(
+                height: 65,
+                width: 65,
+                padding: EdgeInsets.all(8),
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Colors.white,
+                ),
+                child: FloatingActionButton(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  elevation: 0,
+                  onPressed: () {
+                    if (addbackShow) {
+                      _setAddBackshow(false);
+                    } else {
+                      _setAddBackshow(true);
+                    }
 
-        child: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          elevation: 0,
-          onPressed: () {},
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomNavigationBar(
-        items: _myTabs,
-        currentIndex: _currentIndex,
-        onTap: _itemTapped,
-        type: BottomNavigationBarType.fixed,
-        fixedColor: Theme.of(context).primaryColor,
-        backgroundColor: Colors.white,
-      ),
+//            showModalBottomSheet(
+//                context: context,
+//                elevation: 0,
+//                isScrollControlled: true,
+//                backgroundColor: MyColors.alphaWhite,
+//                builder: (BuildContext context) {
+//                  return _addWidget(context);
+//                });
+                  },
+                ),
+              ),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+              bottomNavigationBar: BottomNavigationBar(
+                items: _myTabs,
+                currentIndex: _currentIndex,
+                onTap: _itemTapped,
+                type: BottomNavigationBarType.fixed,
+                fixedColor: Theme.of(context).primaryColor,
+                backgroundColor: Colors.white,
+                elevation: 0,
+              ),
+            )),
+        AnimatedSwitcher(
+          transitionBuilder: (child,anim){
+            return ScaleTransition(scale: anim,child: child);
+          },
+          duration: Duration(milliseconds: 300),
+          child: _addWidget(context),
+        )
+        
+      ],
     );
+  }
+
+  void _setAddBackshow(bool show) {
+    setState(() {
+      addbackShow = show;
+      () => opacityLevel = opacityLevel == 0? 1.0 : 0.0;
+    });
   }
 
   void _itemTapped(int index) {
@@ -104,5 +158,103 @@ class _MyAppPageState extends State<AppPage> {
       _currentIndex = index;
       appBarTitle = tabData[index]['text'];
     });
+  }
+
+  double opacityLevel = 1.0;
+
+  Widget _addWidget(BuildContext context) {
+    if (addbackShow) {
+      return new Scaffold(
+          backgroundColor: Colors.transparent,
+          body: new Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Container(),
+                  new Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                    child: new Container(
+                      height: 120,
+                      child: new CustomScrollView(
+                        physics: BouncingScrollPhysics(),
+                        slivers: <Widget>[
+                          new SliverPadding(
+                            padding: EdgeInsets.all(0.0),
+                            sliver: new SliverList(
+                              delegate: new SliverChildListDelegate(<Widget>[
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    _addIconWidget(context, 0),
+                                    _addIconWidget(context, 1),
+                                    _addIconWidget(context, 2),
+                                    _addIconWidget(context, 3),
+                                  ],
+                                )
+                              ]),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Center(
+                      child: GestureDetector(
+                          child: new Padding(
+                            padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
+                            child: Icon(
+                              Icons.clear,
+                              size: 25,
+                            ),
+                          ),
+                          onTap: () {
+                            _setAddBackshow(false);
+//                      Navigator.pop(context);
+                          }))
+                ],
+              ),
+            ),
+          );
+    } else {
+      return Container(
+        width: 0,
+        height: 0,
+      );
+    }
+  }
+
+  Widget _addIconWidget(BuildContext context, int index) {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          height: 50.0,
+          width: 50.0,
+          padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+          margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            color: addIconColors[index],
+          ),
+          child: addIcon[index],
+        ),
+        new Text(addIconItems[index],
+            style: new TextStyle(
+              color: Colors.black,
+            )),
+      ],
+    );
   }
 }
